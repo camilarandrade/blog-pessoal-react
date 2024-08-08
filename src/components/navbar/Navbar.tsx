299a99ca4f208d62e2ff80223908b6f9cdea79af
Camila Andrade <camila.rda@outlook.com>
2024-08-08 14:17:24 -0300

    Feat: add api connection

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import React from 'react'
+
 
 function Navbar() {
+ 
+  
+
   return (
     <>
-      <div className='w-full bg-pink-200 text-gray-700 flex justify-center p-5'>
-      <div className="container flex justify-between text-lg">
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
             <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
+
             <div className='flex gap-4'>
               <Link to='/login' className='hover:underline'>Login</Link>
               <Link to='/home' className='hover:underline'>Home</Link>
